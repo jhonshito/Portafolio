8195850d9e52dcad9f341030af092033f911bb6d
Jhon <caicedojhon957@gmail.com>
2022-11-16 17:07:48 -0500

    agregue alerta para que el usuario sepa que estoy trabajando en el sitio web

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Descripcion from './components/Descripcion'
 import Habilidades from './components/Habilidades'
 import Inicio from './components/Inicio'
 import Navbar from './components/Navbar'
 import Proyectos from './components/Proyectos'
 
+import Swal from 'sweetalert2'
+
 const App = () => {
+
+  useEffect(() => {
+    Swal.fire({
+      icon: "info",
+      title: "Información inportante",
+      text: "Hola querido ❤ visitante. me gustaria informarte que el propietario de este sitio web actualmente se encuentra trabajando en el si te gustaria ver lo que lleva  dale OK"
+    })
+  },[])
   return (
     <div className='container mx-auto'>
         <Navbar />
